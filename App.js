738a86fe4cc3831169c918020e311dc9ccd856c7
knfjed <knfjed@gmail.com>
2021-01-06 17:01:28 +0900

    mapで展開した配列データをpropsに渡して表示させる

--- App.js
+++ App.js
@@ -2,21 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import ListItem from './components/ListItem';
+import articles from './dummies/articles.json';
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        text={article.title}
+        author={article.author}
+        // keyを忘れずに
+        key={index}
+      />
+    );
+  });
+
   return (
     <View style={styles.container}>
-      <ListItem
-        imageUrl="https://picsum.photos/200"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
-        author="sampleNews"
-      />
+      {/* ↓mapで展開したarticlesの内容を表示 */}
+      {items}
       <StatusBar style="auto" />
     </View>
   );
